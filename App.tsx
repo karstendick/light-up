b55ab527673cdc441d88433bc3d47af24f044253
Joshua Karstendick <joshdick@gmail.com>
2023-12-12 15:51:52 -0500

    got a 2-dimensional grid of buttons

--- App.tsx
+++ App.tsx
@@ -1,16 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import './styles.css'
+
+const tiles = [1, 2, 3, 4]
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={main_style.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+        <div className='board_grid'>
+          {tiles.map((tile) => (
+            <button
+              key={tile}>{tile}</button>
+          ))}
+        </div>
+      </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
+const main_style = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
